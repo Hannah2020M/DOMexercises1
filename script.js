4a9e117ec50c338c9db4d9b8e8b5f8a2a3fa6b44
Hannah2020M <coder2020aug@gmail.com>
2022-03-05 19:40:00 -0600

    added indexHTML scriptJS styleCSS files

---
+++ script.js
@@ -0,0 +1,14 @@
+//https://replit.com/@Hcoder/Exercises-DOM-and-Events#script.js
+//(home)/2022LC101janWork/exercisesLC101/CH22to30cssDOMgitFormsJSONtypeScrAngular/Ch20to23HTMLcssGitDOMhttp/-
+
+function init () {
+    const missionAbort = document.getElementById("abortMission");
+    const button = document.getElementById("liftoffButton");
+    const paragraph = document.getElementById("statusReport");
+
+    // Put your code for the exercises here.
+  document.getElementByid("liftoffButton");
+    
+}
+
+window.addEventListener("load", init);
